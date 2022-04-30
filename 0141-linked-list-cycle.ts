9af2549f7c44b0320d2284d5ade0b64845066ffe
Sheng Hau <shioju12@gmail.com>
2022-04-30 12:22:55 +0800

    add 0141

---
+++ 0141-linked-list-cycle.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+  const visited = new Set();
+  while (head !== null) {
+    if (visited.has(head)) {
+      return true;
+    }
+    visited.add(head);
+    head = head.next;
+  }
+  return false;
+}
